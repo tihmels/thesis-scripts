e3d60673e2d4f5b137882a9d9eabbbbcd939fb37
Tjado Ihmels <frank.tjado.ihmels@uni-oldenburg.de>
2022-05-20 11:17:53 +0200

    frame extraction script done

--- frame_extraction/py_ffmpeg.py
+++ frame_extraction/py_ffmpeg.py
@@ -1,65 +1,73 @@
 #!/Users/tihmels/Scripts/thesis/conda-env/bin/python
 
 import argparse
-import glob
+import multiprocessing as mp
 import os
+import re
 import shutil
 from pathlib import Path
 
 import ffmpeg
 
+TV_FILENAME_RE = r'TV-(\d{8})-(\d{4})-(\d{4}).webs.h264.mp4'
 
-def extract_frames_from_video(video_file: Path, output_dir: Path, fps=0.0):
-    stream = ffmpeg.input(video_file.absolute())
+
+def extract_frames_from_video(video: Path, fps=0.0, overwrite=False, prune=False):
+    match = re.match(TV_FILENAME_RE, video.name)
+
+    if match is None:
+        print(f'{video} does not match TV pattern. Skip ...')
+        return
+
+    output_dir = Path(video.parent, match.group(2))
+
+    if prune and output_dir.exists():
+        shutil.rmtree(output_dir)
+
+    output_dir.mkdir(exist_ok=True)
+
+    stream = ffmpeg.input(video.absolute())
 
     if fps > 0:
         stream = stream.filter('fps', fps=fps)
 
     stream = stream.output(f'{output_dir}/frame_%05d.jpg')
-    stream.run()
+    ffmpeg.run(stream, overwrite_output=overwrite, quiet=True)
 
-
-def extract_frames_from_video_files(video_files, fps=0.0, overwrite=False):
-    videos = list(map(lambda v: Path(v), video_files))
-
-    videos = [video for video in videos if video.is_file() and video.name.endswith('.mp4')]
-
-    for video in videos:
-        parent_dir = video.parent
-        filename = video.name
-        timecode = filename.split("-")[2]
-
-        output_dir = Path(parent_dir, timecode)
-
-        if output_dir.exists() and overwrite:
-            shutil.rmtree(output_dir)
-        elif output_dir.exists() and not overwrite:
-            print(f'skipping {output_dir} because it already exists')
-            continue
-
-        output_dir.mkdir()
-
-        extract_frames_from_video(video, output_dir, fps)
+    return video
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('files', type=str, nargs='+')
-    parser.add_argument('--fps', type=float, default=0.0)
-    parser.add_argument('-r', '--recursive', action='store_true')
-    parser.add_argument('-o', '--overwrite', action='store_true')
+    parser.add_argument('files', type=lambda p: Path(p).resolve(strict=True), nargs='+', help="video files or directories containing video files for frame extraction ")
+    parser.add_argument('--fps', type=float, default=0.0, help="extract frames per second")
+    parser.add_argument('-r', '--recursive', action='store_true', help="search recursively for video files")
+    parser.add_argument('-o', '--overwrite', action='store_true', help="overwrite existing frame files")
+    parser.add_argument('-p', '--prune', action='store_true', help="prune all frame files if output directory exists")
+    parser.add_argument('--parallel', action='store_true', help="execute frame extraction in parallel multiprocessing")
 
     args = parser.parse_args()
 
     files = []
 
     for file in args.files:
-        if os.path.isfile(file) and file.endswith('.mp4'):
+        if file.is_file() and re.match(TV_FILENAME_RE, file.name):
             files.append(file)
-        elif os.path.isdir(file) and not args.recursive:
-            [files.append(f) for f in glob.glob(f'{file}/*.mp4')]
-        elif os.path.isdir(file) and args.recursive:
-            [files.append(f) for f in glob.glob(f'{file}/**/*.mp4')]
+        elif file.is_dir() and not args.recursive:
+            [files.append(f) for f in file.glob('*.mp4') if re.match(TV_FILENAME_RE, os.path.basename(f))]
+        elif file.is_dir() and args.recursive:
+            [files.append(f) for f in file.rglob('*.mp4') if re.match(TV_FILENAME_RE, os.path.basename(f))]
 
-    extract_frames_from_video_files(files, args.fps, args.overwrite)
+    if args.parallel:
+        with mp.Pool(os.cpu_count()) as pool:
+            tasks = [pool.apply_async(extract_frames_from_video, (file,),
+                                      kwds={'fps': args.fps, 'overwrite': args.overwrite, 'prune': args.prune},
+                                      callback=lambda f: print(f'{f.name} done')) for file in files]
+            pool.close()
+            pool.join()
+
+    else:
+        for file in files:
+            extract_frames_from_video(file, args.fps, args.overwrite, args.prune)
+            print(f'{file.name} done')
