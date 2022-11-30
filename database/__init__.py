def766db167a2b3aa96f40c6956d9f50a94146d6
Tjado Ihmels <dev@tihmels.de>
2022-11-30 17:58:39 +0100

    redis upload rdy

--- database/__init__.py
+++ database/__init__.py
@@ -1,5 +1,11 @@
 import redis
 
-red = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+# red = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 
-# red = redis.Redis(host=args.host, port=args.port, db=args.db, decode_responses=True, username='default', password = 'YX0Nx3ddpclPyewTGzvswBnZrPyT9Tit')
+
+red = redis.Redis(host='redis-13975.c91.us-east-1-3.ec2.cloud.redislabs.com',
+                  port=13975,
+                  db=0,
+                  decode_responses=True,
+                  username='default',
+                  password='YX0Nx3ddpclPyewTGzvswBnZrPyT9Tit')
