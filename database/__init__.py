f96370b0bdf61d7c809ab5e04dbc23b441cc4eda
Tjado Ihmels <dev@tihmels.de>
2023-03-05 21:48:09 +0100

    pseudo gen now according to shots

--- database/__init__.py
+++ database/__init__.py
@@ -6,5 +6,7 @@ from walrus import *
 host = os.getenv('REDIS_HOST', default='localhost')
 port = os.getenv('REDIS_PORT', default=6379)
 
+print(f'RedisConn: {host}:{port}')
+
 db = Database(host=host, port=port, db=0, decode_responses=True)
 rai = Client(address=f'{host}:{port}')
